3622c249aee8d7da9134f5599dc08dd7354b00a8
evelant <imagio@mailfence.com>
2024-02-10 11:31:36 -0500

    add platform patch and faster text encoder polyfill, still slow

--- app/client.ts
+++ app/client.ts
@@ -1,7 +1,7 @@
 import * as Http from "@effect/platform/HttpClient";
 import { Resolver } from "@effect/rpc";
 import { HttpResolver } from "@effect/rpc-http";
-import { Console, Effect, Stream } from "effect";
+import { Effect, Stream } from "effect";
 import type { UserRouter } from "../index";
 import { GetUser, GetUserIds } from "./schema";
 
@@ -17,18 +17,13 @@ const client = HttpResolver.make<UserRouter>(
 ).pipe(Resolver.toClient);
 
 // Use the client
-const t1 = performance.now();
-client(new GetUserIds())
-  .pipe(
-    Stream.runCollect,
-    Effect.flatMap(
-      Effect.forEach(id => client(new GetUser({ id })), { batching: true })
-    ),
-    Effect.tap(Console.log),
-    Effect.tapErrorCause(Console.error),
-    Effect.runPromise
-  )
-  .then(() => {
-    const t2 = performance.now();
-    console.log(`done in ${t2 - t1}ms`);
-  });
+client(new GetUserIds()).pipe(
+  Stream.runCollect,
+  Effect.flatMap(
+    Effect.forEach(id => client(new GetUser({ id })), { batching: true })
+  ),
+  Effect.tap(Effect.log),
+  Effect.tapErrorCause(Effect.logError),
+  Effect.withLogSpan(`client`),
+  Effect.runFork
+);
--- app/index.js
+++ app/index.js
@@ -1,16 +1,14 @@
 import { registerRootComponent } from "expo";
 //@ts-expect-error
-// import { polyfill } from "react-native-polyfill-globals";
-// import { polyfill as polyfillEncoding } from "react-native-polyfill-globals/src/encoding";
 import { polyfillGlobal } from "react-native/Libraries/Utilities/PolyfillFunctions";
-
-// import { polyfill as polyfillFetch } from "react-native-polyfill-globals/src/fetch";
-const { TextEncoder, TextDecoder } = require("text-encoding");
+// const { TextEncoder, TextDecoder } = require("fast-text-encoding");
+require("fastestsmallesttextencoderdecoder");
 const { fetch, Headers, Request, Response } = require("react-native-fetch-api");
 const { ReadableStream } = require("web-streams-polyfill");
+// const { ReadableStream } = require("@stardazed/streams");
 
-polyfillGlobal("TextEncoder", () => TextEncoder);
-polyfillGlobal("TextDecoder", () => TextDecoder);
+// polyfillGlobal("TextEncoder", () => TextEncoder);
+// polyfillGlobal("TextDecoder", () => TextDecoder);
 polyfillGlobal("ReadableStream", () => ReadableStream);
 polyfillGlobal("fetch", () => fetch);
 polyfillGlobal("Headers", () => Headers);
